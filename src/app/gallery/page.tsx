0534330aa51ff9c7cf43b1a318e304f3bef9b12a
Adib Grouz <contact@adib-grouz.com>
2024-09-21 15:36:20 +0200

    add navbar and footer

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,21 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
 import { FocusCardsDemo } from "../components/focus-cards";
 
-
-export default function Home() {
+export default function Gallery(className?: string) {
   return (
-    <main className="bg-black text-white min-h-screen">
+    <div
+      className={cn(
+        "relative flex min-h-screen flex-col items-center justify-center overflow-hidden bg-slate-950 w-full rounded-md z-0",
+        className
+      )}
+    >
+
+      <div className="flex flex-col items-center justify-center w-full h-full p-5">
         <FocusCardsDemo />
-    </main>
-  )
+      </div>
+    </div>
+  );
 }
