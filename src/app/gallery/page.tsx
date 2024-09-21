a710741e50d9a83c58e2d27a9a4db2922b845431
Adib Grouz <contact@adib-grouz.com>
2024-09-21 11:43:54 +0200

    add components and update main

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,10 @@
-import { LampText } from "./components/lamp";
+import { FocusCardsDemo } from "../components/focus-cards";
 
 
 export default function Home() {
   return (
     <main className="bg-black text-white min-h-screen">
-      <LampText />
+        <FocusCardsDemo />
     </main>
   )
 }
