2f44a8e977bb090b646a66004584f49fb12d19d2
Adib Grouz <contact@adib-grouz.com>
2024-09-21 16:41:04 +0200

    update pages to modules

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,12 +1,3 @@
-import React from 'react';
-
-const GalleryPage: React.FC = () => {
-    return (
-        <div>
-            <h1>Gallery Page</h1>
-            <p>Welcome to the gallery!</p>
-        </div>
-    );
-};
-
-export default GalleryPage;
+export default function Gallery() {
+  return <></>;
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,3 @@
-import React from 'react';
-
-const ProjectsPage: React.FC = () => {
-    return (
-        <div>
-            <h1>Projects Page</h1>
-        </div>
-    );
-};
-
-export default ProjectsPage;
+export default function Projects() {
+  return <></>;
+}
